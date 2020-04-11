cf21b7d926d9d517a69af4b9503b8b92bb3b5d3c
yuki <inuzuka199@gmail.com>
2020-04-12 00:31:58 +0900

    clean up.

--- buffer.h
+++ buffer.h
@@ -3,17 +3,17 @@
 typedef struct
 {
   char *data;
-  size_t capacity;
-  size_t used;
+  uint32_t capacity;
+  uint32_t used;
 } buffer_t;
 
-static void buffer_init(buffer_t *buf, size_t capacity) {
+static void buffer_init(buffer_t *buf, uint32_t capacity) {
   buf->data = malloc(capacity);
   buf->capacity = capacity;
   buf->used = 0;
 }
 
-static void buffer_reserve(buffer_t *buf, size_t new_capacity) {
+static void buffer_reserve(buffer_t *buf, uint32_t new_capacity) {
   if (buf->capacity >= new_capacity) {
     return;
   }
@@ -27,29 +27,62 @@ static void buffer_reserve(buffer_t *buf, size_t new_capacity) {
   buf->capacity = new_capacity;
 }
 
-static size_t max_size_t(size_t a, size_t b) {
+static uint32_t max_uint32_t(uint32_t a, uint32_t b) {
   return a > b ? a : b;
 }
 
-static void buffer_put(buffer_t *buf, const char *data, size_t data_size) {
+static void buffer_put(buffer_t *buf, const char *data, uint32_t data_size) {
   if (data_size == 0) {
     return;
   }
 
   if (buf->used + data_size > buf->capacity) {
-    size_t new_capacity = max_size_t(2 * buf->capacity, buf->used + data_size);
+    uint32_t new_capacity = max_uint32_t(2 * buf->capacity, buf->used + data_size);
     buffer_reserve(buf, new_capacity);
   }
 
-  for (size_t i = 0; i < data_size; i++) {
+  for (uint32_t i = 0; i < data_size; i++) {
     buf->data[buf->used + i] = data[i];
   }
 
   buf->used += data_size;
 }
 
-static size_t char_index(const char *data, size_t data_size, char c) {
-  for (size_t i = 0; i < data_size; i++) {
+static void buffer_put_until(buffer_t *buf, const char *data, const char *data_end) {
+  bool valid = data <= data_end;
+  if (!valid) {
+    return;
+  }
+
+  uint32_t size = (uint32_t)(data_end - data);
+  buffer_put(buf, data, size);
+}
+
+static void buffer_put_string(buffer_t *buf, const char *data) {
+  for (uint32_t i = 0; data[i] != '\0'; i++) {
+    if (buf->used == buf->capacity) {
+      buffer_reserve(buf, 2 * buf->capacity);
+    }
+    buf->data[buf->used] = data[i];
+    buf->used++;
+  }
+}
+
+static void buffer_shrink_to(buffer_t *buf, const char *to) {
+  bool valid = (buf->data <= to) && (to <= buf->data + buf->used);
+  if (!valid) {
+    return;
+  }
+
+  buf->used = (uint32_t)(to - buf->data);
+}
+
+static void buffer_clear(buffer_t *buf) {
+  buf->used = 0;
+}
+
+static uint32_t char_index(const char *data, uint32_t data_size, char c) {
+  for (uint32_t i = 0; i < data_size; i++) {
     if (data[i] == c) {
       return i;
     }
@@ -69,12 +102,12 @@ static void buffer_get_line(buffer_t *buf, FILE *f) {
   }
   buf->data = result;
 
-  size_t index_of_null = char_index(buf->data, buf->capacity, '\0');
+  uint32_t index_of_null = char_index(buf->data, buf->capacity, '\0');
   bool buffer_too_small = (index_of_null == buf->capacity - 1) && (buf->data[index_of_null - 1] != '\n');
   while (buffer_too_small) {
-    size_t old_buf_size = buf->capacity;
+    uint32_t old_buf_size = buf->capacity;
     buffer_reserve(buf, 2 * buf->capacity);
-    size_t offset = old_buf_size - 1; // minus one for a null character.
+    uint32_t offset = old_buf_size - 1; // minus one for a null character.
     result = fgets(buf->data + offset, (int)(buf->capacity - offset), f);
     if (result == NULL) {
       exit(1);
--- gokuro_c.c
+++ gokuro_c.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
+#include <stdint.h>
 #include <time.h>
 
-#include "buffer.h"
-
+#if __clang__
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wsign-conversion"
 #pragma clang diagnostic ignored "-Wunused-parameter"
 #pragma clang diagnostic ignored "-Wcast-align"
 #pragma clang diagnostic ignored "-Wshadow"
+#endif
+
 #define STB_DS_IMPLEMENTATION
 #include "stb_ds.h"
+
+#if __clang__
 #pragma clang diagnostic pop
+#endif
+
+typedef uint8_t bool;
+#define true 1
+#define false 0
+
+#include "buffer.h"
 
 static bool begin_with(const char *a, const char *b) {
-  size_t i = 0;
+  uint32_t i = 0;
   while (true) {
     if (a[i] == '\0' || b[i] == '\0') {
       return true;
@@ -30,44 +40,39 @@ static bool begin_with(const char *a, const char *b) {
 }
 
 static bool end_with(const char *a, const char c) {
-  size_t i = 0;
+  uint32_t i = 0;
   bool just_read_c = false;
   while (true) {
     if (a[i] == '\0') {
       return just_read_c;
     }
-
     just_read_c = (a[i] == c);
     i++;
   }
 }
 
 typedef struct {
-  size_t name_offset; // redundant
-  size_t name_length; // redundant
-  size_t body_offset;
-  size_t body_length; // redundant
+  uint32_t is_valid;
+  uint32_t body_offset;
 } macro_def;
 
 typedef struct {
-  size_t key;
+  uint64_t key;
   macro_def value;
 } macro_hash_t;
 
-// https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
-static size_t fnv1_hash_64(const char *byte, size_t length)
+static uint64_t hash(void *data_begin, void *data_end)
 {
-  size_t hash = 1099511628211u;
-  for (size_t i = 0; i < length; i++)
+  uint64_t hash = 1099511628211u;
+  for (unsigned char *c = data_begin; c < (unsigned char *)data_end; c++)
     {
-      hash *= 14695981039346656037u;
-      hash ^= (size_t)(byte[i]);
+      hash = hash * 33 + *c;
     }
   return hash;
 }
 
 int main() {
-  const size_t buffer_initial_size = 4;
+  const uint32_t buffer_initial_size = 4;
   buffer_t line_buf = {0};
   buffer_init(&line_buf, buffer_initial_size);
   if (line_buf.data == NULL) {
@@ -93,7 +98,7 @@ int main() {
   }
 
   time_t t = time(NULL);
-  stbds_rand_seed((size_t)(t));
+  stbds_rand_seed((uint32_t)(t));
 
   macro_hash_t *macros = NULL;
 
@@ -105,189 +110,199 @@ int main() {
 
     if (!end_with(line_buf.data, '\n')) {
       // This line is the last line of the input.
-      buffer_put(&line_buf, "\n", 1);
+      line_buf.data[line_buf.used - 1] = '\n'; // '\0' -> '\n'
+      buffer_put(&line_buf, "", 1);
     }
 
-    if (begin_with(line_buf.data, "#+MACRO: ")) {
-      // This line may define a macro.
-      const size_t line_length = line_buf.used - 2;
-      const size_t name_offset = 9; // 9 = strlen("#+MACRO: ")
-      const size_t invalid = line_length + 1;
+    char *line_begin = line_buf.data;
+    char *line_end = line_buf.data + line_buf.used - 2; // 2 = strlen("\n\0")
 
-      if (line_buf.data[name_offset] == '\n') {
+    if (begin_with(line_begin, "#+MACRO: ")) {
+      // This line may define a macro.
+      const uint32_t name_offset = 9; // 9 = strlen("#+MACRO: ")
+
+      if (*(line_begin + name_offset) == '\n') {
         break;
       }
 
-      size_t name_length = 0;
-      while (true) {
-        if (line_buf.data[name_offset + name_length] == ' ') {
-          break;
+      char *name_begin = line_begin + name_offset;
+      char *name_end = NULL;
+      { // read the name of the macro.
+        char *c = name_begin;
+        while (true) {
+          if (*c == ' ') {
+            name_end = c;
+            break;
+          }
+          if (*c == '\n') {
+            // This line does not define a macro.
+            break;
+          }
+          c++;
         }
-        if (line_buf.data[name_offset + name_length] == '\n') {
-          // This line does not define a macro.
-          name_length = invalid;
-          break;
+
+        if (name_end == NULL) {
+          fprintf(stdout, "%s", line_begin);
+          continue;
         }
-        name_length++;
       }
 
-      if (name_length == invalid) {
-        fprintf(stdout, "%s", line_buf.data);
-        continue;
-      }
-
-      buffer_put(&macro_names, line_buf.data + name_offset, name_length);
+      buffer_put_until(&macro_names, name_begin, name_end);
       buffer_put(&macro_names, "", 1);
 
-      size_t body_offset = name_offset + name_length + 1; // 1 = strlen(" ")
-      size_t body_length = line_length - body_offset;
-      buffer_put(&macro_bodies, line_buf.data + body_offset, body_length);
+      buffer_put_until(&macro_bodies, name_end + 1, line_end);
       buffer_put(&macro_bodies, "", 1);
 
-      macro_def m = {macro_names.used - 1 - name_length, name_length, macro_bodies.used - 1 - body_length, body_length};
-      size_t macro_name_hash = fnv1_hash_64(line_buf.data + name_offset, name_length);
+      uint32_t body_length = (uint32_t)(line_end - name_end) - 1; // 1 = strlen(" ")
+      macro_def m = {true, macro_bodies.used - body_length - 1}; // 1 = strlen("\0")
+      uint64_t macro_name_hash = hash(name_begin, name_end);
       hmput(macros, macro_name_hash, m);
 
-      fprintf(stdout, "%s", line_buf.data);
+      fprintf(stdout, "%s", line_begin);
       continue;
     }
 
     // macro expansion
     while (true) {
-      const size_t line_length = line_buf.used - 2;
-      const size_t bbb_offset = 3; // 3 = strlen("{{{") or strlen("}}}")
-      const size_t invalid_index = line_length + 1;
-      size_t macro_begin = invalid_index;
-      size_t macro_end = invalid_index;
-      if (true) {
-        // find the last macro call.
-        size_t i = line_length;
+      const uint32_t bbb_offset = 3; // 3 = strlen("{{{") or strlen("}}}")
+      line_begin = line_buf.data;
+      line_end = line_buf.data + line_buf.used - 2; // 2 = strlen("\n\0")
+
+      char *macro_begin = NULL;
+      char *macro_end = NULL;
+      { // find the last macro call.
+        char *c = line_end; // *c == "\n"
         while (true) {
-          if (i < bbb_offset - 1) {
+          if (c < line_begin) {
             break;
           }
-          if (line_buf.data[i] == '{') {
-            i--;
-            if (line_buf.data[i] == '{') {
-              i--;
-              if (line_buf.data[i] == '{') {
-                macro_begin = i;
+          if (*c == '{') {
+            c--;
+            if (*c == '{') {
+              c--;
+              if (*c == '{') {
+                macro_begin = c;
                 break;
               }
             }
-          } else if (line_buf.data[i] == '}') {
-            i--;
-            if (line_buf.data[i] == '}') {
-              i--;
-              if (line_buf.data[i] == '}') {
-                if (i == 0) {
-                  macro_end = invalid_index;
-                  break;
-                }
-                macro_end = i + 3;
-                i--;
+          } else if (*c == '}') {
+            c--;
+            if (*c == '}') {
+              c--;
+              if (*c == '}') {
+                macro_end = c + 3;
+                c--;
                 continue;
               }
             }
           } else {
-            i--;
+            c--;
           }
         }
-      }
 
-      if (macro_begin == invalid_index || macro_end == invalid_index) {
-        // no macro to expand.
-        break;
-      }
-
-      size_t name_sep = macro_begin;
-      while (true) {
-        if (line_buf.data[name_sep] == '(' || line_buf.data[name_sep] == '}') {
+        if (macro_begin == NULL || macro_end == NULL) {
+          // no macro to expand.
           break;
         }
-        name_sep++;
       }
-      const char *macro_name = line_buf.data + macro_begin + bbb_offset;
-      size_t name_length = name_sep - macro_begin - bbb_offset;
-      size_t macro_name_hash = fnv1_hash_64(macro_name, name_length);
-      macro_def m = hmget(macros, macro_name_hash); // m may be a zero value.
 
-      if (m.name_length == 0) {
+      char *name_begin = macro_begin + bbb_offset;
+      char *name_end = NULL;
+      bool isConstant;
+      { // get the length of the macro name.
+        char *c = name_begin;
+        while (true) {
+          if (*c == '(') {
+            isConstant = false;
+            break;
+          }
+
+          if (*c == '}') {
+            isConstant = true;
+            break;
+          }
+          c++;
+        }
+        name_end = c;
+      }
+
+      // lookup the definition of the macro.
+      uint64_t name_hash = hash(name_begin, name_end);
+      macro_def m = hmget(macros, name_hash);
+
+      if (!m.is_valid) {
         // the macro is undefined.
-        line_buf.used = macro_begin;
-        size_t line_rest_size = line_length - macro_end + 2; // 2 = strlen("\n\0")
-        buffer_put(&line_buf, line_buf.data + macro_end, line_rest_size);
+        buffer_shrink_to(&line_buf, macro_begin);
+        buffer_put_until(&line_buf, macro_end, line_end + 2); // 2 = strlen("\n\0")
         continue;
       }
 
-      bool isConstant = (macro_end - macro_begin - 2 * bbb_offset) == name_length;
       if (isConstant) {
         // temp_buf = line[macro_end:]
-        temp_buf.used = 0;
-        buffer_put(&temp_buf, line_buf.data + macro_end, line_length - macro_end + 2); // 2 = strlen("\n\0")
+        buffer_clear(&temp_buf);
+        buffer_put_until(&temp_buf, macro_end, line_end + 2); // 2 = strlen("\n\0")
 
         // line = line[:macro_begin] + macro_body + line[maro_end:]
-        line_buf.used = macro_begin;
-        buffer_put(&line_buf, macro_bodies.data + m.body_offset, m.body_length);
+        buffer_shrink_to(&line_buf, macro_begin);
+        buffer_put_string(&line_buf, macro_bodies.data + m.body_offset);
         buffer_copy(&line_buf, &temp_buf);
-      } else if (!isConstant) {
+      } else {
         // parse the arguments.
-        size_t argOffsets[11] = {0};
-        argOffsets[0] = macro_begin + bbb_offset + m.name_length + 1; // 1 = strlen("(")
-        argOffsets[1] = macro_begin + bbb_offset + m.name_length + 1; // 1 = strlen("(")
-        size_t currentIndex = 2;
-        for (size_t i = argOffsets[1]; i < macro_end - bbb_offset; i++) {
+        char *args[11] = {0};
+        args[0] = name_end + 1; // 1 = strlen("(")
+        args[1] = name_end + 1; // 1 = strlen("(")
+        uint32_t currentIndex = 2;
+        for (char *c = args[1]; c < macro_end; c++) {
           if (currentIndex == 10) {
             break;
           }
-          if (line_buf.data[i] == ',') {
-            argOffsets[currentIndex] = i + 1;
+          if (*c == ',') {
+            args[currentIndex] = c + 1; // 1 = strlen(",")
             currentIndex++;
           }
         }
-        argOffsets[currentIndex] = macro_end - bbb_offset;
+        args[currentIndex] = macro_end - bbb_offset;
 
-        // Expand the macro on temp_buf (we cannot modify line_buf because argOffsets depends on it).
-        temp_buf.used = 0;
-        size_t writeFrom = m.body_offset;
-        size_t writeUntil = m.body_offset;
+        // Expand the macro on temp_buf (we cannot modify line_buf because args depends on it).
+        buffer_clear(&temp_buf);
+        buffer_put_until(&temp_buf, line_begin, macro_begin);
+        char *writeFrom = macro_bodies.data + m.body_offset;
+        char *c = macro_bodies.data + m.body_offset;
         while (true) {
-          if (macro_bodies.data[writeUntil] == '\0') {
-            buffer_put(&temp_buf, macro_bodies.data + writeFrom, writeUntil - writeFrom);
+          if (*c == '\0') {
+            buffer_put_until(&temp_buf, writeFrom, c);
             break;
           }
-          if (macro_bodies.data[writeUntil] == '$') {
-            // If macro_bodies.data[writeUntil] != '\n', we can read macro_bodies.data[writeUntil + 1].
-            writeUntil++;
-            if ('0' <= macro_bodies.data[writeUntil] && macro_bodies.data[writeUntil] <= '9') {
-              size_t offset = 1; // 1 = strlen("$")
-              buffer_put(&temp_buf, macro_bodies.data + writeFrom, writeUntil - writeFrom - offset);
-              size_t argIndex = (size_t)(macro_bodies.data[writeUntil]) - '0';
-              size_t writeSize = 0;
+          if (*c == '$') {
+            // If *c != '\n', we can read *(c + 1).
+            c++;
+            if ('0' <= *c && *c <= '9') {
+              uint32_t offset = 1; // 1 = strlen("$")
+              buffer_put_until(&temp_buf, writeFrom, c - offset);
+              uint32_t argIndex = (uint32_t)(*c - '0');
+              char *writeUntil;
               if (argIndex == 0) {
                 // The replacement is the whole text in the brackets.
-                writeSize = macro_end - macro_begin - m.name_length - 2 * bbb_offset - 2; // 2 = strlen("()")
+                writeUntil = macro_end - bbb_offset - 1; // 1 = strlen(")")
               } else {
-                writeSize = argOffsets[argIndex + 1] - argOffsets[argIndex] - 1; // 1 = strlen(",") or strlen(")")
+                writeUntil = args[argIndex + 1] - 1; // 1 = strlen(",") or strlen(")")
               }
-              buffer_put(&temp_buf, line_buf.data + argOffsets[argIndex], writeSize);
-              writeUntil++;
-              writeFrom = writeUntil;
+              buffer_put_until(&temp_buf, args[argIndex], writeUntil);
+              c++;
+              writeFrom = c;
               continue;
             }
           }
-          writeUntil++;
+          c++;
         }
-        size_t macro_expanded_size = temp_buf.used;
-        size_t line_rest_size = line_length - macro_end + 2; // 2 = strlen("\n\0")
-        buffer_put(&temp_buf, line_buf.data + macro_end, line_rest_size);
 
-        line_buf.used = macro_begin;
-        buffer_put(&line_buf, temp_buf.data, macro_expanded_size);
-        buffer_put(&line_buf, temp_buf.data + macro_expanded_size, line_rest_size);
+        // copy the expanded line to line_buf.
+        buffer_put_until(&temp_buf, macro_end, line_end + 2); // 2 = strlen("\n\0")
+        line_buf.used = 0;
+        buffer_copy(&line_buf, &temp_buf);
       }
     }
-    fprintf(stdout, "%s", line_buf.data);
+    fprintf(stdout, "%s", line_begin);
   }
 
   free(macro_names.data);
